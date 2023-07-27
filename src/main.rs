d80cb5aff7bd4a8a55e6576bdf43f5ea4ba04b5e
Neil Jav <j.nmka.g@gmail.com>
2023-07-26 22:47:16 -0600

    feat: add serde and cargo watch to cargo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)] // for start only
+use std::net::SocketAddr;
+
+use axum::{
+    extract::Query,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use serde::Deserialize;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("-> Listening on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+// region: --- handler hello
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("-->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World");
+    Html(format!("Hello <strong> {name}!!!</strong>"))
 }
